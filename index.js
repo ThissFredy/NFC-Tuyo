93533d49574c455e34e49b61b8b34323eabaef27
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2025-09-15 19:15:42 -0500

    🐛 Link bug

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-  // Intenta abrir la aplicación de Spotify de inmediato.
-  window.location = "spotify:track:4tQofG51E0juZBBVr6pral?si=wcs-tEPJQfytzfvCccCGGQ";
+function openSpotify() {
+  // URI de la app
+  const spotifyUri = "spotify:track:4tQofG51E0juZBBVr6pral";
+  
+  // URL de respaldo para la web
+  const webUrl = "https://open.spotify.com/intl-es/track/4tQofG51E0juZBBVr6pral?si=c1e550bc363540af";
 
-  // Si la app no se abre después de 2 segundos, redirige al sitio web como plan B.
+  // Intenta abrir la URI.
+  window.location = spotifyUri;
+
+  // El fallback ahora es menos necesario, pero lo dejamos por si acaso.
   setTimeout(function() {
-      window.location = "https://open.spotify.com/intl-es/track/4tQofG51E0juZBBVr6pral?si=c1e550bc363540af";
-  }, 2000);
+      // Si después de 2.5s el usuario no ha cambiado de página, 
+      // es probable que no tenga la app, lo mandamos a la web.
+      window.location = webUrl;
+  }, 2500);
+}
